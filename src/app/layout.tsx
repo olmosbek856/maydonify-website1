6646fb42bf07bef42584ce70d1d1d16f22038378
Olmosbek Rustamov <o.rustamov@newuu.uz>
2026-04-16 14:49:53 +0500

    add icon when search on google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ const sora = Sora({
 });
 
 export const metadata: Metadata = {
-  title: "Maydon - Futbol uchun mini stadionlarni masofadan bron qilish platformasi",
+  title: "Maydon - Futbol maydonlarini masofadan bron qilish platformasi.",
   description:
     "Toshkentdagi 30+ tasdiqlangan futbol maydonlarini toping va 30 soniyada band qiling. Hech qanday qo'ng'iroqsiz. Real vaqt jadval, aniq narxlar, tezkor bron.",
   keywords: [
@@ -53,10 +53,12 @@ export const metadata: Metadata = {
       { url: "/favicon.ico", sizes: "any" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
+    shortcut: "/favicon.ico",
   },
 };
 
